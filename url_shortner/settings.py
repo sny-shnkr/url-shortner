a1c8e1abee836b457a42bef456c64922dcf4c519
Sunny Shankar <sunny.shankar44@gmail.com>
2022-02-20 23:19:43 +0530

    Update settings.py
    Remove secret key from settings file

--- url_shortner/settings.py
+++ url_shortner/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_c(0h+^188tn)2sh6ycidgi7nsrptkjpyrum%8#+nd$gg-q&e6'
-
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
